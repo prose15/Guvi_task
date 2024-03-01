7af939b76b08a2a9a24f0b8143faabef036e21a6
Prosper Martial <125741217+prose15@users.noreply.github.com>
2024-03-01 19:13:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
        {
-        origin: [""],
+        origin: ["https://guvi-task-frontend-seven.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
